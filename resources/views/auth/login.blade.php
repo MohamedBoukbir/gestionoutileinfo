48270f20621a282d8aebd3c1646447147c41c98e
ChaymaaMez <mezouarachaimaa@gmail.com>
2023-11-04 02:03:06 +0100

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,231 +7,167 @@
 	<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 	<title>se connecter</title>
 	<style>
-body{
-	margin:0;
-	color:#6a6f8c;
-	background-image: url("/images/salle.jpg");
-	background-position: center;
-	background-size: cover;
-	backdrop-filter: blur(5px);
-	font:600 16px/18px 'Open Sans',sans-serif;
+		@import url('https://fonts.googleapis.com/css?family=Raleway:400,700');
+body {
+	background: linear-gradient(90deg, #bfbbd9, #776BCC);		
 }
-*,:after,:before{box-sizing:border-box}
-.clearfix:after,.clearfix:before{content:'';display:table}
-.clearfix:after{clear:both;display:block}
-a{color:inherit;text-decoration:none}
-
-.login-wrap{
-	width:100%;
-	margin:auto;
-	max-width:525px;
-	height:100vh;
-	position:relative;
-	background:url(https://raw.githubusercontent.com/khadkamhn/day-01-login-form/master/img/bg.jpg) no-repeat center;
-	box-shadow:0 12px 15px 0 rgba(0,0,0,.24),0 17px 50px 0 rgba(0,0,0,.19);
+* {
+	box-sizing: border-box;
+	margin: 0;
+	padding: 0;	
+	font-family: Raleway, sans-serif;
 }
-.login-html{
-	width:100%;
-	height:100%;
-	position:absolute;
-	padding:70px 70px 50px 70px;
-	background:rgba(40,57,101,.9);
+.container {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	min-height: 100vh;
 }
-.login-html .sign-in-htm,
-.login-html .sign-up-htm{
-	top:0;
-	left:0;
-	right:0;
-	bottom:0;
-	position:absolute;
-	transform:rotateY(180deg);
-	backface-visibility:hidden;
-	transition:all .4s linear;
+.screen {		
+	background: linear-gradient(90deg, #5D54A4, #7C78B8);		
+	position: relative;	
+	height: 600px;
+	width: 360px;	
+	box-shadow: 0px 0px 24px #5C5696;
 }
-.login-html .sign-in,
-.login-html .sign-up,
-.login-form .group .check{
-	display:none;
+.screen__content {
+	z-index: 1;
+	position: relative;	
+	height: 100%;
 }
-.login-html .tab,
-.login-form .group .label,
-.login-form .group .button{
-	text-transform:uppercase;
+.screen__background {		
+	position: absolute;
+	top: 0;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	z-index: 0;
+	-webkit-clip-path: inset(0 0 0 0);
+	clip-path: inset(0 0 0 0);	
 }
-.login-html .tab{
-	font-size:22px;
-	margin-right:15px;
-	padding-bottom:5px;
-	margin:0 15px 10px 0;
-	display:inline-block;
-	border-bottom:2px solid transparent;
-}
-.login-html .sign-in:checked + .tab,
-.login-html .sign-up:checked + .tab{
-	color:#fff;
-	border-color:#1161ee;
-}
-.login-form{
-	min-height:345px;
-	position:relative;
-	perspective:1000px;
-	transform-style:preserve-3d;
-	margin-top: 40px;
-}
-.login-form .group{
-	margin-bottom:15px;
-}
-.login-form .group .label,
-.login-form .group .input,
-.login-form .group .button{
-	width:100%;
-	color:#fff;
-	display:block;
-}
-.login-form .group .input,
-.login-form .group .button{
-	border:none;
-	padding:15px 20px;
-	border-radius:25px;
-	background:rgba(255,255,255,.1);
-}
-.login-form .group input[data-type="password"]{
-	text-security:circle;
-	-webkit-text-security:circle;
-}
-.login-form .group .label{
-	color:#aaa;
-	font-size:12px;
-}
-.login-form .group .button{
-	background:#1161ee;
-}
-.login-form .group label .icon{
-	width:15px;
-	height:15px;
-	border-radius:2px;
-	position:relative;
-	display:inline-block;
-	background:rgba(255,255,255,.1);
-}
-.login-form .group label .icon:before,
-.login-form .group label .icon:after{
-	content:'';
-	width:10px;
-	height:2px;
-	background:#fff;
-	position:absolute;
-	transition:all .2s ease-in-out 0s;
-}
-.login-form .group label .icon:before{
-	left:3px;
-	width:5px;
-	bottom:6px;
-	transform:scale(0) rotate(0);
-}
-.login-form .group label .icon:after{
-	top:6px;
-	right:0;
-	transform:scale(0) rotate(0);
-}
-.login-form .group .check:checked + label{
-	color:#fff;
-}
-.login-form .group .check:checked + label .icon{
-	background:#1161ee;
-}
-.login-form .group .check:checked + label .icon:before{
-	transform:scale(1) rotate(45deg);
-}
-.login-form .group .check:checked + label .icon:after{
-	transform:scale(1) rotate(-45deg);
-}
-.login-html .sign-in:checked + .tab + .sign-up + .tab + .login-form .sign-in-htm{
-	transform:rotate(0);
-}
-.login-html .sign-up:checked + .tab + .login-form .sign-up-htm{
-	transform:rotate(0);
+.screen__background__shape {
+	transform: rotate(45deg);
+	position: absolute;
 }
 
-.tab{
+.screen__background__shape1 {
+	height: 520px;
+	width: 520px;
+	background: #FFF;	
+	top: -50px;
+	right: 120px;	
+	border-radius: 0 72px 0 0;
+}
+
+.screen__background__shape2 {
+	height: 220px;
+	width: 220px;
+	background: #6C63AC;	
+	top: -172px;
+	right: 0;	
+	border-radius: 32px;
+}
+
+.screen__background__shape3 {
+	height: 540px;
+	width: 190px;
+	background: linear-gradient(270deg, #5D54A4, #6A679E);
+	top: -24px;
+	right: 0;	
+	border-radius: 32px;
+}
+
+.screen__background__shape4 {
+	height: 400px;
+	width: 200px;
+	background: #7E7BB9;	
+	top: 420px;
+	right: 50px;	
+	border-radius: 60px;
+}
+form {
+	width: 320px;
+	padding: 30px;
+	padding-top: 156px;
+}
+.login__field {
+	padding: 20px 0px;	
+	position: relative;	
+}
+.login__input {
+	border: none;
+	border-bottom: 2px solid #D1D1D4;
+	background: none;
+	padding: 10px;
+	padding-left: 24px;
+	font-weight: 700;
+	width: 75%;
+	transition: .2s;
+}
+
+.login__input:active,
+.login__input:focus,
+.login__input:hover {
+	outline: none;
+	border-bottom-color: #6A679E;
+}
+.login__submit {
+	background: #fff;
+	font-size: 14px;
+	margin-top: 30px;
+	padding: 16px 20px;
+	border-radius: 26px;
+	border: 1px solid #D4D3E8;
+	text-transform: uppercase;
+	font-weight: 700;
+	display: flex;
+	align-items: center;
+	width: 100%;
+	color: #4C489D;
+	box-shadow: 0px 2px 2px #5C5696;
 	cursor: pointer;
+	transition: .2s;
 }
-
-.input{
-	margin-bottom: 20px;
-}
-
-.input:focus{
+.login__submit:active,
+.login__submit:focus,
+.login__submit:hover {
+	border-color: #6A679E;
 	outline: none;
 }
-
-.hr{
-	height:2px;
-	margin:60px 0 50px 0;
-	background:rgba(255,255,255,.2);
-}
-.foot-lnk, .foot-lnk label{
-	text-align:center;
-	cursor: pointer;
-}
-.foot-lnk:hover{
-	color: white;
-}
-
-.label{
-	margin-bottom: 13px;
-}
-
-.button{
-	cursor: pointer;
-}
-
 
 	</style>
 </head>
 <body>
-	<div class="login-wrap">
-	<div class="login-html">
-		<div style=" display: flex;
-		justify-content: center;
-		align-items: center;">
-			
-		</div>
-
-		
-		<input id="tab-1" type="radio" name="tab" class="sign-in" checked><h3 style="font-size: 14px" class="tab">Se Connecter</h3>
-		<input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab"></label>
-		<div class="login-form">
-			<div class="sign-in-htm">
+	<div class="container">
+		<div class="screen">
+			<div class="screen__content">
 				<form action="{{ route('login') }}" method="POST">
 					@csrf
-					<div class="group">
-						<label for="email" class="label">Email </label>
-						<input id="email" type="text" name="email" class="input" required autofocus>
+					<div class="login__field">
+						<input id="email" type="text" name="email" class="login__input" placeholder="User name / Email" required autofocus>
 						@error('email')
                             <span class="invalid-feedback"  style="color: red;" role="alert">
                                 <strong>{{ $message }}</strong>
                             </span>
                         @enderror
 					</div>
-					<div class="group">
-						<label for="pass" class="label">Mot De Passe</label>
-						<input id="pass" name="password" type="password" class="input @error('password') is-invalid @enderror" data-type="password" required>
+					<div class="login__field">
+						<input id="pass" name="password" type="password" class="login__input @error('password') is-invalid @enderror" data-type="password" placeholder="Password" required>
 					@error('password')
 						<span class="invalid-feedback"  style="color: red;" role="alert">
 							<strong>{{ $message }}</strong>
 						</span>
 					@enderror
-					</div>
-				
-					<div class="group">
-						<input type="submit" class="button" value="Se Connecter">
-					</div>
-					<div class="hr"></div>
-			
+					</div>				
+						<input type="submit" class="login__submit" value="Se Connecter"><i class="button__icon fas fa-chevron-right"></i>
 				</form>
 			</div>
-			
+			<div class="screen__background">
+				<span class="screen__background__shape screen__background__shape4"></span>
+				<span class="screen__background__shape screen__background__shape3"></span>		
+				<span class="screen__background__shape screen__background__shape2"></span>
+				<span class="screen__background__shape screen__background__shape1"></span>
+			</div>	
 		</div>
 	</div>
 </div>
